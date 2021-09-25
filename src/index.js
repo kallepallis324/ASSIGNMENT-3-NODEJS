609daefc58a3a4b5b6ab16da182c27e1d5934cff
kallepallis324 <84132122+kallepallis324@users.noreply.github.com>
2021-09-25 23:14:52 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import {Store,persistedStore} from './Redux/Store/Store'
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <PersistGate loading={null} persistor={persistedStore}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
